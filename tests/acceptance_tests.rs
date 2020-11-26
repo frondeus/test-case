5725571a20cb9dfd452af153f99b5d034f5248e9
Łukasz Biel <lukasz.p.biel@gmail.com>
2020-11-26 11:00:58 +0100

    Remove backwards tests for rust < 1.41

--- tests/acceptance_tests.rs
+++ tests/acceptance_tests.rs
@@ -1,6 +1,7 @@
 #![cfg(test)]
 
 mod acceptance {
+    use itertools::Itertools;
     use std::env;
     use std::path::PathBuf;
     use std::process::Command;
@@ -13,7 +14,11 @@ mod acceptance {
             .output()
             .expect("cargo command failed to start");
 
-        let lines = String::from_utf8_lossy(&output.stdout).to_string();
+        let lines = String::from_utf8_lossy(&output.stdout)
+            .to_string()
+            .lines()
+            .sorted()
+            .join("\n");
         insta::assert_display_snapshot!(lines);
     }
 
@@ -25,23 +30,27 @@ mod acceptance {
             .output()
             .expect("cargo command failed to start");
 
-        let lines = String::from_utf8_lossy(&output.stdout).to_string();
+        let lines = String::from_utf8_lossy(&output.stdout)
+            .to_string()
+            .lines()
+            .sorted()
+            .join("\n");
         insta::assert_display_snapshot!(lines);
     }
 
     #[test]
     fn r#async() {
-        if !env::var("TEST_CASE_ACCEPTANCE_RUN_ASYNC").map_or(true, |v| v == "true") {
-            return; // Test can be skipped via environment variable, when ran on rust < 1.41.0
-        }
-
         let output = Command::new("cargo")
             .current_dir(PathBuf::from("acceptance_tests").join("async"))
             .args(&["test"])
             .output()
             .expect("cargo command failed to start");
 
-        let lines = String::from_utf8_lossy(&output.stdout).to_string();
+        let lines = String::from_utf8_lossy(&output.stdout)
+            .to_string()
+            .lines()
+            .sorted()
+            .join("\n");
         insta::assert_display_snapshot!(lines);
     }
 }
