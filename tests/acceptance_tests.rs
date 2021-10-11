20952ff96dd94efc4b2889f7443a798507640703
Tony Duan <tony84727@gmail.com>
2021-10-11 16:48:28 +0800

    Separate insta snapshots by toolchain version/channel

--- tests/acceptance_tests.rs
+++ tests/acceptance_tests.rs
@@ -1,10 +1,19 @@
 #![cfg(test)]
 
 mod acceptance {
+    use insta::with_settings;
     use itertools::Itertools;
     use std::env;
     use std::path::PathBuf;
     use std::process::{Command, Output};
+
+    fn get_snapshot_directory() -> String {
+        PathBuf::from("snapshots")
+            .join(env::var("SNAPSHOT_DIR").unwrap_or("rust-stable".to_string()))
+            .to_str()
+            .unwrap()
+            .to_string()
+    }
 
     fn retrieve_stdout(output: &Output) -> String {
         String::from_utf8_lossy(&output.stdout)
@@ -21,37 +30,43 @@ mod acceptance {
 
     #[test]
     fn basic() {
-        let output = Command::new("cargo")
-            .current_dir(PathBuf::from("acceptance_tests").join("basic"))
-            .args(&["test"])
-            .output()
-            .expect("cargo command failed to start");
+        with_settings!({snapshot_path => get_snapshot_directory()}, {
+            let output = Command::new("cargo")
+                .current_dir(PathBuf::from("acceptance_tests").join("basic"))
+                .args(&["test"])
+                .output()
+                .expect("cargo command failed to start");
 
-        let lines = retrieve_stdout(&output);
-        insta::assert_display_snapshot!(lines);
+            let lines = retrieve_stdout(&output);
+            insta::assert_display_snapshot!(lines);
+        });
     }
 
     #[test]
     fn hamcrest_assertions() {
+        with_settings!({snapshot_path => get_snapshot_directory()}, {
         let output = Command::new("cargo")
-            .current_dir(PathBuf::from("acceptance_tests").join("hamcrest_assertions"))
-            .args(&["test"])
-            .output()
-            .expect("cargo command failed to start");
+                    .current_dir(PathBuf::from("acceptance_tests").join("hamcrest_assertions"))
+                    .args(&["test"])
+                    .output()
+                    .expect("cargo command failed to start");
 
-        let lines = retrieve_stdout(&output);
-        insta::assert_display_snapshot!(lines);
+                let lines = retrieve_stdout(&output);
+                insta::assert_display_snapshot!(lines);
+        });
     }
 
     #[test]
     fn r#async() {
-        let output = Command::new("cargo")
-            .current_dir(PathBuf::from("acceptance_tests").join("async"))
-            .args(&["test"])
-            .output()
-            .expect("cargo command failed to start");
+        with_settings!({snapshot_path => get_snapshot_directory()}, {
+            let output = Command::new("cargo")
+                .current_dir(PathBuf::from("acceptance_tests").join("async"))
+                .args(&["test"])
+                .output()
+                .expect("cargo command failed to start");
 
-        let lines = retrieve_stdout(&output);
-        insta::assert_display_snapshot!(lines);
+            let lines = retrieve_stdout(&output);
+            insta::assert_display_snapshot!(lines);
+        });
     }
 }
