9e59b4eae553141971eece78edf8d288a465033c
Łukasz Biel <lukasz.p.biel@gmail.com>
2021-12-19 15:47:47 +0100

    Remove CI directory

--- ci/common.bash
+++
@@ -1,6 +0,0 @@
-required_arg() {
-    if [ -z "$1" ]; then
-        echo "Required argument $2 missing"
-        exit 1
-    fi
-}
--- ci/set_rust_version.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-rustup default $1
-rustup target add $2
