f848378bc5fd23421c1deccbf4ecbd60df09ac80
Łukasz Biel <lukasz.p.biel@gmail.com>
2022-01-28 00:14:07 +0100

    Bump MSRV to 1.49. Adjust template. Release 2.0.0-rc2

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ cargo +nightly clippy --all-targets --all-features -- -D warnings
 cargo +nightly fmt --all
 SNAPSHOT_DIR=rust-stable cargo +stable test --workspace --all-features
 SNAPSHOT_DIR=rust-nightly cargo +nightly test --workspace --all-features
-SNAPSHOT_DIR=rust-1.41.0 cargo +1.41 test --workspace --all-features
+SNAPSHOT_DIR=rust-1.49.0 cargo +1.49 test --workspace --all-features
 
 nvim Cargo.toml
 cargo build
@@ -24,7 +24,9 @@ git add .
 git commit
 git push origin
 
-echo "Next step: Wait for CI"
-echo "Next step: \`git tag vX.Y.Z; git push --tags\`"
-echo "Next step: Create release in Github"
-echo "Next step: \`cargo publish\`"
+set +o xtrace
+
+echo "Next step: Wait for CI" \
+"Next step: \`git tag vX.Y.Z; git push --tags\`" \
+"Next step: Create release in Github" \
+"Next step: \`cargo publish\`"
