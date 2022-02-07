f3ddeecaa74746ea315970ceb3f1ec41365ff80e
Łukasz Biel <lukasz.p.biel@gmail.com>
2022-02-07 13:28:02 +0100

    Print stderr in async (need to debug on gh)

--- tests/acceptance_tests.rs
+++ tests/acceptance_tests.rs
@@ -74,6 +74,7 @@ mod acceptance {
                 .expect("cargo command failed to start");
 
             let lines = retrieve_stdout(&output);
+            eprintln!("{}", retrieve_stderr(&output));
             insta::assert_display_snapshot!(lines);
         });
     }
