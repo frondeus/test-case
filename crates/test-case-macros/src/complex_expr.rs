a8770538579848d5dfbb3cfaf08a7cafd213afd4
Łukasz Biel <lukasz.p.biel@gmail.com>
2022-07-21 11:11:08 +0200

    chore: Lock crates targeting MSRV >1.49

--- crates/test-case-macros/src/complex_expr.rs
+++ crates/test-case-macros/src/complex_expr.rs
@@ -38,7 +38,7 @@ mod kw {
     syn::custom_keyword!(matches_regex);
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum OrderingToken {
     Eq,
     Lt,
@@ -47,52 +47,52 @@ pub enum OrderingToken {
     Geq,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum PathToken {
     Any,
     Dir,
     File,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Ord {
     pub token: OrderingToken,
     pub expected_value: Box<Expr>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct AlmostEqual {
     pub expected_value: Box<Expr>,
     pub precision: Box<Expr>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Path {
     pub token: PathToken,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Contains {
     pub expected_element: Box<Expr>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct ContainsInOrder {
     pub expected_slice: Box<Expr>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Len {
     pub expected_len: Box<Expr>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Count {
     pub expected_len: Box<Expr>,
 }
 
 #[cfg(feature = "with-regex")]
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Regex {
     pub expected_regex: Box<Expr>,
 }
