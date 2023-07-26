0d5aac92ff4ee007d238cf319958d27951fde822
Ross Williams <ross@ross-williams.net>
2023-07-26 15:53:58 -0400

    Add test_matrix macro acceptance tests
    Test basic features and for correct compilation error
    output

---
+++ tests/acceptance_cases/matrices_compilation_errors/src/lib.rs
@@ -0,0 +1,21 @@
+#![cfg(test)]
+use test_case::test_matrix;
+
+#[test_matrix(
+    ["one", 1, true,]
+)]
+fn mixed_literals(x: u32) {
+    unreachable!("Should never compile")
+}
+
+const END: u32 = 1;
+
+#[test_matrix(1..END)]
+fn non_literal_range(x: u32) {
+    unreachable!("Should never compile")
+}
+
+#[test_matrix(1..)]
+fn unbounded_range(x: u32) {
+    unreachable!("Should never compile")
+}
